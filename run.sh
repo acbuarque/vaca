a0f5c9736876f0cb5e5cea3ffe5b8d704c80820c
acbuarque <aurelio.filho@postclick.com>
2021-02-16 20:32:55 -0300

    main

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@
 
 list=$(cat bla.txt)
 for p in $list; do
-	echo "current p: $p"
         sanitized_package_name=${p#@*/}
-	echo "sanitized_package_name $sanitized_package_name"
         is_package_stage_only=$(cat my-file.txt | grep -c $sanitized_package_name)
         if [[ $is_package_stage_only -ne 0 ]]; then
-        	continue
+        	echo "skiping promotion of $sanitized_package_name"
+		continue
         fi
 	echo "deploying $sanitized_package_name"
 done
