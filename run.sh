984458e09a5877aacc3352035b32e8801c8a75fd
acbuarque <aurelio.filho@postclick.com>
2021-02-16 20:30:08 -0300

    chore: fix

--- run.sh
+++ run.sh
@@ -4,12 +4,11 @@ list=$(cat bla.txt)
 for p in $list; do
 	echo "current p: $p"
         sanitized_package_name=${p#@*/}
-        cat my-file.txt | grep drone-convert-service
-        cat my-file.txt | grep 'vaca'
-        echo "sanitized_package_name $sanitized_package_name"
+	echo "sanitized_package_name $sanitized_package_name"
         is_package_stage_only=$(cat my-file.txt | grep -c $sanitized_package_name)
         if [[ $is_package_stage_only -ne 0 ]]; then
         	continue
         fi
+	echo "deploying $sanitized_package_name"
 done
 
