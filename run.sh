56cc2ea0da4efcc8a35efbda0519d86275798996
acbuarque <aurelio.filho@postclick.com>
2021-02-16 21:37:02 -0300

    chore: using sed

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 list=$(cat bla.txt)
 for p in $list; do
-        sanitized_package_name=${p#@*/}
+        sanitized_package_name=$(echo $p | sed 's/@postclick\///') #${p#@*/}
         is_package_stage_only=$(cat my-file.txt | grep -c $sanitized_package_name)
         if [[ $is_package_stage_only -ne 0 ]]; then
         	echo "skiping promotion of $sanitized_package_name"
