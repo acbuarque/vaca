e10933bc0cf9d0bea48f9e97f4ce7d526788f37d
acbuarque <aurelio.filho@postclick.com>
2021-02-17 06:40:51 -0300

    hehe

--- entry-point.sh
+++ entry-point.sh
@@ -6,12 +6,12 @@ ls $PWD
 
 echo "hi"
 
-if [[ -z "$PLUGIN_PACKAGES_TO_PROMOTE_FILE_PATH" ]]; then
-	echo "file with packages to promote not provided"
-	exit 1
-else
-	export PACKAGES_TO_PROMOTE=$(cat $PACKAGES_TO_PROMOTE_FILE_PATH)
-fi
+#if [[ -z "$PLUGIN_PACKAGES_TO_PROMOTE_FILE_PATH" ]]; then
+#	echo "file with packages to promote not provided"
+#	exit 1
+#else
+#	export PACKAGES_TO_PROMOTE=$(cat $PACKAGES_TO_PROMOTE_FILE_PATH)
+#fi
 
-echo $PACKAGES_TO_PROMOTE
+#echo $PACKAGES_TO_PROMOTE
 
